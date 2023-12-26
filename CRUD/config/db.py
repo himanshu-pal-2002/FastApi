2bfe3e3ccec1f0c591e0bbdb070136316921055f
Himanshu Pal <palhimanshu206243@gmail.com>
2023-12-26 12:43:08 +0530

    fastapi

--- CRUD/config/db.py
+++ CRUD/config/db.py
@@ -1,6 +1,9 @@
 from sqlalchemy import create_engine
 
-engine=create_engine("mysql+pymysql://root@localhost:3306/test")
+# engine=create_engine("mysql+pymysql://root:your_password@localhost:3306/test")
 # engine=create_engine("sqlite:///crud.db")
+engine = create_engine('mysql+mysqlconnector://root:Him20@12@localhost:3306/test')
 
 conn = engine.connect()
+
+# engine = create_engine('mysql+mysqlconnector://root:Him20@12@localhost/test')
